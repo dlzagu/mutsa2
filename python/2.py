b992d3ea6c24eb22d8941218bb09c08a1c43eeb8
dlzagu <eodnsdlekd@naver.com>
2022-05-15 22:48:42 +0900

    20220515:파이썬 과제

---
+++ python/2.py
@@ -0,0 +1,5 @@
+hurdle = 5
+while(hurdle>0):
+    print('허들이 %d개 남았습니다.' %hurdle)
+    hurdle -= 1
+print('레이스를 완주했습니다.')
---
+++ python/4.py
@@ -0,0 +1,16 @@
+
+
+for i in range(0, 4):
+    name = input("이름을 입력하세요:")
+    score = int(input("점수를 입력하세요 :"))
+    if score >= 90 :
+        print(name, "의 학점 : A" )
+    elif score >= 80 :
+        print(name, "의 학점 : B" )
+    elif score >= 70 :
+        print(name, "의 학점 : C" )
+    elif score >= 60 :
+        print(name, "의 학점 : D" )
+    else :
+        print(name, "의 학점 : F" )
+
